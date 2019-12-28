8e33a4857d51bfc4c87e69c5e7643407f34df8ab
dannythompson901 <dannythompson901@gmail.com>
2019-12-28 16:55:43 -0600

    react todo app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 import './App.css';
+import Header from './components/layout/Header';
 
-function App() {
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'take out the trash',
+        completed: false
+      },
+      {
+        id:2,
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id:3,
+        title: 'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+  }
+
+  //delete todo
+  delTodo = (id) =>{
+    this.setState({todos: [...this.state.todos.filter(todo=> todo.id !== id)] });
+  }
+
+
+  //Add todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: 4,
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  }
+  render() {
   return (
+  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+      <Header />
+      <AddTodo addTodo={this.addTodo}/>
+      <Todos todos={this.state.todos} markComplete={this.markComplete} 
+      delTodo = {this.delTodo}/>
+    </div>
     </div>
   );
 }
-
+}
 export default App;
